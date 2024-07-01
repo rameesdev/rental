7ef095816a487a4956a51ad8c971c1b746ff776e
rameesdev <muhammadramees697@gmail.com>
2024-07-01 21:07:17 +0530

    v0.0.2

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cookieParser = require("cookie-parser")
 const jwt = require("jsonwebtoken")
-path = require("path")
+const path = require("path")
+const fs = require("fs")
 require("dotenv").config()
 const MONGODB_URI = process.env.URI;
 const uploadsPath = path.join(__dirname, 'uploads');
